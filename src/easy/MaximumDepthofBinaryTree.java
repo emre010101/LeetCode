782d81d9e22c80ba596fa8b023cc8fdbf91a16c4
Emre <emrekavak3938@gmail.com>
2023-05-30 17:35:22 +0100

    depth and bbst

---
+++ src/easy/MaximumDepthofBinaryTree.java
@@ -0,0 +1,16 @@
+package easy;
+import java.util.*;
+
+public class MaximumDepthofBinaryTree {
+	
+    public int maxDepth(TreeNode root) {
+    	if(root == null) {
+    		return 0;
+    	}else {
+    		int leftD = maxDepth(root.left);
+    		int righD = maxDepth(root.right);
+    		return Math.max(leftD, righD)+1;
+    	}
+    }
+
+}
