fc2627e150fbefae14c94a98c417a76a128a24b7
Emre <emrekavak3938@gmail.com>
2023-06-11 12:15:06 +0100

    SQL
    Inner Join + Left Join Quieries

---
+++ bin/SQL/leftjoin.sql
@@ -0,0 +1,16 @@
+package SQL;
+
+
+	
+	/*Write an SQL query to report the first name, last name, city,
+	 *  and state of each person in the Person table. 
+	 *  If the address of a personId is not present in the Address table, report null instead.
+	 *  Return the result table in any order.
+	 * */
+	
+SELECT Person.firstName, Person.lastName, Address.city, Address.state
+FROM Person
+LEFT JOIN Address ON Person.personId = Address.personId;
+
+
+
